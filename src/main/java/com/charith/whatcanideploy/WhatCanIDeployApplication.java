0cd0816f2441f515b852906c811a20dff984e44c
Charith Tangirala <charithtangirala@users.noreply.github.com>
2019-10-05 06:55:46 -0500

    Initial commit

---
+++ src/main/java/com/charith/whatcanideploy/WhatCanIDeployApplication.java
@@ -0,0 +1,13 @@
+package com.charith.whatcanideploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatCanIDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatCanIDeployApplication.class, args);
+    }
+
+}
